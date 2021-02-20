e652c0c24aadde6c986bb7fcc7b5f832a21585ff
KILIAN MAGNIEZ <kilian.magniez@viacesi.fr>
2021-02-20 20:22:34 +0100

    ajout user id

--- BattleCalculator.Model/Entities/Game.cs
+++ BattleCalculator.Model/Entities/Game.cs
@@ -7,6 +7,7 @@ namespace BattleCalculator.Model.Entities
     public class Game : IEntityBase
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
         public int Level { get; set; }
         public long Chrono { get; set; }
         public int TotalScore { get; set; }
